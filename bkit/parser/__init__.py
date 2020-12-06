f90a81a35fa93c5b3529bdb7584a126dba156121
Pham Hoang Hai <hai.pham.hoang@hcmut.edu.vn>
2020-12-06 08:34:09 +0700

    Initial code for assignment 1

---
+++ bkit/parser/__init__.py
@@ -0,0 +1,1 @@
+from .antlr_build import *
---
+++ bkit/parser/antlr_build/__init__.py
@@ -0,0 +1,6 @@
+from .BKITLexer import BKITLexer
+from .BKITParser import BKITParser
+try:
+    from .BKITVisitor import BKITVisitor
+except ImportError:
+    pass
